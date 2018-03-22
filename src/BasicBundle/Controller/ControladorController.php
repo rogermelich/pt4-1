1a4c26df3202efe48578165a116b4bd31b0bb0dc
Roger Melich <rogermelich@gmail.com>
2018-03-22 15:41:09 +0100

    Theme Ok

--- src/BasicBundle/Controller/ControladorController.php
+++ src/BasicBundle/Controller/ControladorController.php
@@ -3,7 +3,31 @@
 namespace BasicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ControladorController extends Controller
 {
+     public function variableAction(Request $request)
+    {
+        return $this->render('BasicBundle:Default:var.html.twig', array(
+            'var' => "Hey !!! ".$request->get("var"),
+        ));
+    }
+    
+    public function aboutAction()
+    {
+        return $this->render('BasicBundle:Default:about.html.twig');
+    }
+    
+    public function productsAction()
+    {
+        return $this->render('BasicBundle:Default:products.html.twig');
+    }
+    
+    public function storeAction()
+    {
+        return $this->render('BasicBundle:Default:store.html.twig');
+    }
+    
+    
 }
